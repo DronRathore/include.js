23445475676b05b2500fb783d68809c4964e69e5
Dron Rathore <dron.rathore@gmail.com>
2013-04-09 14:22:18 +0530

    Hmm..

---
+++ include.js
@@ -0,0 +1,13 @@
+/*
+	include.js
+	@authour: Dron Rathore
+	@version: 0.1b
+	@depends: OmJs ver 0.1+
+*/
+
+function require(resourceUrl,fileType,throughAjax,headers){
+	if(!throughAjax){
+		var head=document.getElementById("head");
+		filetype=
+	}
+}
